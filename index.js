09a57760efb57e25f04e06a050f05362b40b46a9
Kyle Olsen <kyle.g.olsen@ey.com>
2020-08-28 10:18:46 -0700

    Fixing some errors, letting eslint flag others

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // simple Node 'Hello World' HTTP server
+/* eslint no-var: "error" */
+/* eslint-env es6 */
 
 'use strict'
 
 var http = require('http')
+var hostname = 'some-server.com'
 var port = 8080
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
+})
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
