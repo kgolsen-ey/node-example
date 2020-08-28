7815b2c035f5d456492122c4a1d1d06c3291adf6
Kyle Olsen <kyle.g.olsen@ey.com>
2020-08-28 10:05:03 -0700

    first code commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-example
+
+![Node.js CI](https://github.com/kgolsen-ey/node-example/workflows/Node.js%20CI/badge.svg)
---
+++ index.js
@@ -0,0 +1,16 @@
+// simple Node 'Hello World' HTTP server
+
+'use strict';
+
+var http = require('http');
+var port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
